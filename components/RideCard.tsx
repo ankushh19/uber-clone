455ca67b3384f1f8821ffff10656a30e8792c86b
ankushh19 <ak9108824@gmail.com>
2024-10-15 14:54:56 +0530

    added complete backend

---
+++ components/RideCard.tsx
@@ -0,0 +1,36 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { Ride } from "@/types/type";
+
+const RideCard = ({
+  ride: {
+    destination_latitude,
+    destination_longitude,
+    origin_address,
+    destination_address,
+    created_at,
+    ride_time,
+    driver,
+    payment_status,
+  },
+}: {
+  ride: Ride;
+}) => {
+  return (
+    <View className="flex flex-row items-center justify-center bg-white rounded-lg shadow-sm shadow-neutral-300 mb-3">
+      <View className="flex flex-row items-center justify-between p-3">
+        <View className="flex flex-row items-center justify-between">
+          <Image
+            source={{
+              uri: `https://maps.geoapify.com/v1/staticmap?style=osm-bright&width=600&height=400&center=lonlat:${destination_longitude},${destination_latitude}&zoom=14&apiKey=${process.env.EXPO_PUBLIC_GEOAPIFY_API_KEY}`,
+            }}
+            className="w-[80px] h-[90px] rounded-lg"
+          />
+        </View>
+      </View>
+      <Text className="text-3xl">{driver.first_name}</Text>
+    </View>
+  );
+};
+
+export default RideCard;

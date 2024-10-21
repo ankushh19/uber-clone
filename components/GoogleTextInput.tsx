902d40f972997e8ca971cec79d8e0cab4cd83e58
ankushh19 <ak9108824@gmail.com>
2024-10-21 15:20:26 +0530

    added riders

--- components/GoogleTextInput.tsx
+++ components/GoogleTextInput.tsx
@@ -1,6 +1,7 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { GoogleInputProps } from "@/types/type";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 const GoogleTextInput = ({
   icon,
@@ -12,7 +13,40 @@ const GoogleTextInput = ({
   <View
     className={`flex flex-row items-center justify-center relative z-50 rounded-xl ${containerStyle} mb-5`}
   >
-    <Text>Search</Text>
+    <GooglePlacesAutocomplete
+      fetchDetails={true}
+      placeholder="Where you want to go?"
+      debounce={200}
+      styles={{
+        textInputContainer: {
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: 20,
+          marginHorizontal: 20,
+          position: "relative",
+          shadowColor: "#d4d4d4",
+        },
+        textInput: {
+          backgroundColor: textInputBackgroundColor || "white",
+          fontSize: 16,
+          marginTop: 5,
+          width: "100%",
+          borderRadius: 200,
+        },
+        listView: {
+          backgroundColor: textInputBackgroundColor || "white",
+          position: "relative",
+          top: 0,
+          width: "100%",
+          borderRadius: 10,
+          shadowColor: "#d4d4d4",
+          zIndex: 99,
+        },
+      }}
+      onPress={(data, details = null) => {
+        handlePress({});
+      }}
+    />
   </View>
 );
 
